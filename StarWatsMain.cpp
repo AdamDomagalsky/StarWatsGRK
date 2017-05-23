c439419634804d9ab841beceec88bb20ad92d176
AdamDomagalsky <adadom@st.amu.edu.pl>
2017-05-23 20:05:52 +0100

    Camera fix, define degresToRads, change floats postions xyz to glm::vec3, light fix etc...

--- StarWatsMain.cpp
+++ StarWatsMain.cpp
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
+#include <glm.hpp>
 #ifdef _WIN32
 #	include <windows.h>
 #else
@@ -19,8 +21,11 @@
 #endif
 //#include "texture.h"
 #define VIEWING_DISTANCE_MIN  3.0
+#define VIEWING_CAMERA_ANGLE_MIN -50
+#define VIEWING_CAMERA_ANGLE_MAX 50
 #define TEXTURE_ID_CUBE 1
-
+#define M_PI 3.14159265358979323846f
+#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
 
 enum {
 	MENU_LIGHTING = 1,
@@ -40,77 +45,54 @@ static GLfloat g_fTeapotAngle2 = 0.0;
 static GLfloat g_fViewDistance = 3 * VIEWING_DISTANCE_MIN;
 static GLfloat g_nearPlane = 1;
 static GLfloat g_farPlane = 1000;
+glm::vec3 pos;
 //Camera params
-static GLfloat xpos = 0, ypos = 0, zpos = 0, xrot = 0, yrot = 0, angle = 0;
-static GLfloat cRadius = 10.0f;
-static GLfloat positionz[10];
-static GLfloat positionx[10];
+static glm::vec3 g_Pos; // Current coordinates x y z
+static glm::vec2 g_Rot; // Current rotation x y
+static GLfloat g_angle = 0;								// our radius distance from our character
+static GLfloat g_ZpositionArr[10], g_XpositionArr[10];
+static glm::vec2 g_lastPos;	// Last coordinates
 
-static GLfloat lastx, lasty;
+static int g_Width = 600;                          // Initial window width
+static int g_Height = 600;                         // Initial window height
+static int g_yClick = 0;
+static float g_lightPos[4] = { 10, 100, -100, 1 };  // Position of light
+#ifdef _WIN32
+static DWORD last_idle_time;
+#else
+static struct timeval last_idle_time;
+#endif
 
+/*Procedular scene starts here*/
 void cubepositions(void) { //set the positions of the cubes
 
 	for (int i = 0;i<10;i++)
 	{
-		positionz[i] = rand() % 5 + 1;
-		positionx[i] = rand() % 5 + 1;
+		g_ZpositionArr[i] = rand() % 5 + 1;
+		g_XpositionArr[i] = rand() % 5 + 1;
 	}
 }
 
-//draw the cube
+//drawing cubes
 void cube(void) {
 	for (int i = 0;i<10 - 1;i++)
 	{
 		glPushMatrix();
-		glTranslated(-positionx[i + 1] * 10, 0, -positionz[i + 1] *
+		glTranslated(-g_XpositionArr[i + 1] * 10, 0, -g_ZpositionArr[i + 1] *
 			10); //translate the cube
 		glutSolidCube(2); //draw the cube
 		glPopMatrix();
 	}
 }
 
-static int g_Width = 600;                          // Initial window width
-static int g_Height = 600;                         // Initial window height
-static int g_yClick = 0;
-static float g_lightPos[4] = { 10, 10, -100, 1 };  // Position of light
-#ifdef _WIN32
-static DWORD last_idle_time;
-#else
-static struct timeval last_idle_time;
-#endif
-void DrawCubeFace(float fSize)
-{
-	fSize /= 2.0;
-	glBegin(GL_QUADS);
-	glVertex3f(-fSize, -fSize, fSize);    glTexCoord2f(0, 0);
-	glVertex3f(fSize, -fSize, fSize);     glTexCoord2f(1, 0);
-	glVertex3f(fSize, fSize, fSize);      glTexCoord2f(1, 1);
-	glVertex3f(-fSize, fSize, fSize);     glTexCoord2f(0, 1);
-	glEnd();
-}
-void DrawCubeWithTextureCoords(float fSize)
-{
-	glPushMatrix();
-	DrawCubeFace(fSize);
-	glRotatef(90, 1, 0, 0);
-	DrawCubeFace(fSize);
-	glRotatef(90, 1, 0, 0);
-	DrawCubeFace(fSize);
-	glRotatef(90, 1, 0, 0);
-	DrawCubeFace(fSize);
-	glRotatef(90, 0, 1, 0);
-	DrawCubeFace(fSize);
-	glRotatef(180, 0, 1, 0);
-	DrawCubeFace(fSize);
-	glPopMatrix();
-}
+/*Whole scene*/
 void RenderObjects(void)
 {
 	cube();
-	float colorBronzeDiff[4] = { 0.8, 0.6, 0.0, 1.0 };
-	float colorBronzeSpec[4] = { 1.0, 1.0, 0.4, 1.0 };
-	float colorBlue[4] = { 0.0, 0.2, 1.0, 1.0 };
-	float colorNone[4] = { 0.0, 0.0, 0.0, 0.0 };
+	GLfloat colorBronzeDiff[4] = { 0.8, 0.6, 0.0, 1.0 };
+	GLfloat colorBronzeSpec[4] = { 1.0, 1.0, 0.4, 1.0 };
+	GLfloat colorBlue[4] = { 0.0, 0.2, 1.0, 1.0 };
+	GLfloat colorNone[4] = { 0.0, 0.0, 0.0, 0.0 };
 	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
 	// Main object (cube) ... transform to its coordinates, and render
@@ -121,7 +103,7 @@ void RenderObjects(void)
 	glMaterialfv(GL_FRONT, GL_SPECULAR, colorNone);
 	glColor4fv(colorBlue);
 	glBindTexture(GL_TEXTURE_2D, TEXTURE_ID_CUBE);
-	DrawCubeWithTextureCoords(1.0);
+	glutSolidCube(1.0);
 	// Child object (teapot) ... relative transform, and render
 	glPushMatrix();
 	glTranslatef(2, 0, 0);
@@ -135,43 +117,33 @@ void RenderObjects(void)
 	glPopMatrix();
 	glPopMatrix();
 }
+
 void display(void)
-{/*
+{
 	// Clear frame buffer and depth buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Set up viewing transformation, looking down -Z axis
+
 	glLoadIdentity();
-	gluLookAt(0, 0, -g_fViewDistance, 0, 0, -1, 0, 1, 0);
-	// Set up the stationary light
-	glLightfv(GL_LIGHT0, GL_POSITION, g_lightPos);
+	/*TODO ship model*/
+	//glTranslatef(0.0f, 0.0f, -g_cRadius);
+	glTranslatef(0.0f, 0.0f, -g_fViewDistance); // 0,0,0 if we want 1st person camera
+	glRotatef(g_Rot.x, 1.0, 0.0, 0.0);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glutSolidCube(1); //Our character to follow
+
+	glRotatef(g_Rot.y, 0.0, 1.0, 0.0);  //rotate our camera on the 	y - axis(up and down)
+	glTranslated(-g_Pos.x, 0.0f, -g_Pos.z); //translate the screen	to the position of our camera
+	glColor3f(1.0f, 1.0f, 1.0f);
+		
 	// Render the scene
 	RenderObjects();
-	// Make sure changes appear onscreen
-	glutSwapBuffers();*/
 
-	glClearColor(0.0, 0.0, 0.0, 1.0); //clear the screen to black
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//clear the color buffer and the depth buffer
-
-
-	glLoadIdentity();
-
-	glTranslatef(0.0f, 0.0f, -cRadius);
-	glRotatef(xrot, 1.0, 0.0, 0.0);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glutSolidCube(2); //Our character to follow
-
-	glRotatef(yrot, 0.0, 1.0, 0.0);  //rotate our camera on the 	y - axis(up and down)
-	glTranslated(-xpos, 0.0f, -zpos); //translate the screen	to the position of our camera
-	glColor3f(1.0f, 1.0f, 1.0f);
-	
+	// Set up the stationary light
 	glLightfv(GL_LIGHT0, GL_POSITION, g_lightPos);
-	
-	RenderObjects();
-	cube(); //call the cube drawing function
 
+	// Make sure changes appear onscreen
 	glutSwapBuffers(); //swap the buffers
-	angle++; //increase the angle
+	g_angle++; //increase the angle
 }
 void reshape(GLint width, GLint height)
 {
@@ -194,6 +166,10 @@ void InitGraphics(void)
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	cubepositions();
+
+	/*Textures loading here*/
+
+
 	// Create texture for cube; load marble texture from file and bind it
 	/*pTextureImage = read_texture("marble.rgb", &width, &height, &nComponents);
 	glBindTexture(GL_TEXTURE_2D, TEXTURE_ID_CUBE);
@@ -221,13 +197,6 @@ void MouseButton(int button, int state, int x, int y)
 }
 void MouseMotion(int x, int y)
 {
-	int diffx = x - lastx; //check the difference between the 	current x and the last x position
-	int diffy = y - lasty; //check the difference between the 	current y and the last y position
-	lastx = x; //set lastx to the current x position
-	lasty = y; //set lasty to the current y position
-	xrot += (float)diffy; //set the xrot to xrot with the addition	of the difference in the y position
-	yrot += (float)diffx;
-	/*
 	// If button1 pressed, zoom in/out if mouse is moved up/down.
 	if (g_bButton1Down)
 	{
@@ -235,7 +204,30 @@ void MouseMotion(int x, int y)
 		if (g_fViewDistance < VIEWING_DISTANCE_MIN)
 			g_fViewDistance = VIEWING_DISTANCE_MIN;
 		glutPostRedisplay();
-	}*/
+		std::cout << g_fViewDistance;
+	}
+	else
+	{
+		int diffx = x - g_lastPos.x; //check the difference between the 	current x and the last x position
+		int diffy = y - g_lastPos.y; //check the difference between the 	current y and the last y position
+		g_lastPos.x = x; //set lastx to the current x position
+		g_lastPos.y = y; //set lasty to the current y position
+		//set the xrot to xrot with the addition	of the difference in the y position
+		//x camera rotation limits
+		if ( g_Rot.x > VIEWING_CAMERA_ANGLE_MAX ){
+			g_Rot.x = VIEWING_CAMERA_ANGLE_MAX;
+		}
+		else if (g_Rot.x < VIEWING_CAMERA_ANGLE_MIN)
+		{
+			g_Rot.x = VIEWING_CAMERA_ANGLE_MIN;
+		}
+		else
+		{
+			g_Rot.x += (float)diffy;
+		}
+		g_Rot.y += (float)diffx;
+	}	
+	std::cout << pos.x << std::endl;
 }
 void AnimateScene(void)
 {
@@ -293,54 +285,50 @@ void Keyboard(unsigned char key, int x, int y)
 	GLfloat xrotrad, yrotrad;
 	switch (key)
 	{
-	case 'q':
-		
-			xrot += 1;
-			if (xrot >360) xrot -= 360;
+		case 'q':
+			g_Rot.x += 1;
+				if (g_Rot.x >360) g_Rot.x -= 360;
 			break;
-
-	case 'z':
-			xrot -= 1;
-			if (xrot < -360) xrot += 360;
-
-		break;
-	case 'w':
-			yrotrad = (yrot / 180 * 3.141592654f);
-			xrotrad = (xrot / 180 * 3.141592654f);
-			xpos += float(sin(yrotrad));
-			zpos -= float(cos(yrotrad));
-			ypos -= float(sin(xrotrad));
-		
-		break;
-	case 's':
-			yrotrad = (yrot / 180 * 3.141592654f);
-			xrotrad = (xrot / 180 * 3.141592654f);
-			xpos -= float(sin(yrotrad));
-			zpos += float(cos(yrotrad));
-			ypos += float(sin(xrotrad));
+		case 'z':
+			g_Rot.x -= 1;
+			if (g_Rot.x < -360) g_Rot.x += 360;
 			break;
-	case 'd':
-			yrotrad = (yrot / 180 * 3.141592654f);
-			xpos += float(cos(yrotrad)) * 0.2;
-			zpos += float(sin(yrotrad)) * 0.2;
+		case 'w':
+			yrotrad = degreesToRadians(g_Rot.y);
+			xrotrad = degreesToRadians(g_Rot.x);
+			g_Pos.x += float(sin(yrotrad));
+			g_Pos.z -= float(cos(yrotrad));
+			g_Pos.y -= float(sin(xrotrad));
 			break;
-	case 'a':
-			yrotrad = (yrot / 180 * 3.141592654f);
-			xpos -= float(cos(yrotrad)) * 0.2;
-			zpos -= float(sin(yrotrad)) * 0.2;
+		case 's':
+			yrotrad = degreesToRadians(g_Rot.y);
+			xrotrad = degreesToRadians(g_Rot.x);
+			g_Pos.x -= float(sin(yrotrad));
+			g_Pos.z += float(cos(yrotrad));
+			g_Pos.y += float(sin(xrotrad));
 			break;
-	case 27:            // ESCAPE key
-		exit(0);
-		break;
-	case 'l':
-		SelectFromMenu(MENU_LIGHTING);
-		break;
-	case 'p':
-		SelectFromMenu(MENU_POLYMODE);
-		break;
-	case 't':
-		SelectFromMenu(MENU_TEXTURING);
-		break;
+		case 'd':
+			yrotrad = degreesToRadians(g_Rot.y);
+			g_Pos.x += float(cos(yrotrad)) * 0.2;
+			g_Pos.z += float(sin(yrotrad)) * 0.2;
+			break;
+		case 'a':
+			yrotrad = degreesToRadians(g_Rot.y);
+			g_Pos.x -= float(cos(yrotrad)) * 0.2;
+			g_Pos.z -= float(sin(yrotrad)) * 0.2;
+			break;
+		case 27:            // ESCAPE key
+			exit(0);
+			break;
+		case 'l':
+			SelectFromMenu(MENU_LIGHTING);
+			break;
+		case 'p':
+			SelectFromMenu(MENU_POLYMODE);
+			break;
+		case 't':
+			SelectFromMenu(MENU_TEXTURING);
+			break;
 	}
 }
 int BuildPopupMenu(void)
@@ -367,8 +355,8 @@ int main(int argc, char** argv)
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(Keyboard);
 	glutMouseFunc(MouseButton);
-	//glutMotionFunc(MouseMotion);
-	glutPassiveMotionFunc(MouseMotion);
+	glutMotionFunc(MouseMotion);// to zoom in 'n' out from model
+	glutPassiveMotionFunc(MouseMotion); // to move around camera
 	glutIdleFunc(AnimateScene);
 	// Create our popup menu
 	BuildPopupMenu();
